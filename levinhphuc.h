228e23c071d9b4a8d04d38f158295040ca79f400
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-05-31 00:38:06 +0300

    Update levinhphuc.h

--- levinhphuc.h
+++ levinhphuc.h
@@ -56,7 +56,7 @@ There is another file for RTOS configuration named OS_CFG.H that have all the st
 #include <stdint.h>			/*FreeRTOS, as explained above*/
 #include "spi.h"			/*include of the serial peripheral interface*/
 #include "stm32l1xx,h"			/*include the header file for STM32L152RE */
-#include "nucleo152start.h"			/*Header file for STM32L152RE*/
+#include "nucleo152start.h"         /*Header file for STM32L152RE*/
 
 /*The Real-Time Kernel (by Jean J. Labrosse) for WIN32 PORT & LINUX PORT*/
 #ifndef __GNUC__
